dc70f5472af06703b7a4f3158366bf2a1e1f6be6
Hassan Ali <hassan@turingtechnologies.org>
2022-10-02 18:20:07 +0500

    comment exercise

--- FavoriteNumber/index.js
+++ FavoriteNumber/index.js
@@ -1,2 +1,16 @@
-let fav_number = 7;
-console.log(`My favorite number is ${fav_number}`)
+var person_name = 'Eric'
+// Name paragraph references
+var lowercaseNameRef = document.getElementById("lowercase_name");
+var uppercaseNameRef = document.getElementById("uppercase_name");
+var titlecaseNameRef = document.getElementById("titlecase_name");
+
+// converting to lowercase
+person_name = person_name.toLowerCase();
+lowercaseNameRef.innerText = person_name;
+// converting to uppercase
+person_name = person_name.toUpperCase();
+uppercaseNameRef.innerText = person_name;
+
+// converting to titlecase
+titlecaseNameRef.innerText = person_name.charAt(0).toUpperCase() +
+person_name.substr(1).toLowerCase();
